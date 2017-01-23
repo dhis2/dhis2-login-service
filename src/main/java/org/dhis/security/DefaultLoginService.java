f2107e09f36b938c627129d689352e2a8e560d7c
Lars Helge Øverland <lars@dhis2.org>
2017-01-23 14:22:41 +0100

    Java8

--- src/main/java/org/dhis/security/DefaultLoginService.java
+++ src/main/java/org/dhis/security/DefaultLoginService.java
@@ -14,18 +14,19 @@ public class DefaultLoginService
     private final Cache<String, Integer> USERNAME_LOGIN_ATTEMPTS_CACHE = Caffeine.newBuilder()
         .build(); //TODO Hint: you might want to provide additional configuration of this cache
     
-    private final int MAX_ATTEMPTS = 5;
-        
+    @Override
     public void registerAuthenticationFailure( AuthenticationEvent event )
     {
         // TODO Implement this method        
     }
 
+    @Override
     public void registerAuthenticationSuccess( AuthenticationEvent event )
     {
         // TODO Implement this method        
     }
 
+    @Override
     public boolean isBlocked( User user )
     {
         // TODO Implement this method        
